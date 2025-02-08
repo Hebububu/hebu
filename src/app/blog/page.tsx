9a5db7d1f5fbea3c2467b8e7164a62e711d9f9a5
hebu <2rwin_rommel@naver.com>
2025-02-08 23:29:34 +0900

    블로그 페이지 컴포넌트 추가: 기본 레이아웃 및 링크 구현

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Blog() {
+    return (
+        <main className= "bg-gray-800 min-h-screen text-white">
+            <Link href= "https://x.com/Hebu_VRC">Hebu</Link>
+        </main>
+    );
+}

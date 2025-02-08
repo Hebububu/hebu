9ba1e1cd0bbf1431ea13401609fac9170ad78c78
hebu <2rwin_rommel@naver.com>
2025-02-08 21:59:37 +0900

    메타데이터 정의 및 네비게이션 바 구성: 링크 목록 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
 import "./globals.css";
 import Link from "next/link";
 
+// 메타데이터 정의
 export const metadata = {
   title: "헤부의 블로그",
   description: "헤부의 블로그에 오신 것을 환영합니다.",
 };
 
+// 네비게이션 링크 목록
+const navItems = [
+  { href: "/", label: "홈" },
+  { href: "/about", label: "소개" },
+  { href: "/blog", label: "블로그" },
+]
+
+
+// 최상위 레이아웃 구성
 export default function RootLayout({
   children,
 }: {
@@ -17,8 +27,13 @@ export default function RootLayout({
       <body>
         <header className="bg-gray-900 text-white">
           <nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
+            <ul className="flex space-x-4 p-4">
+              {navItems.map(({ href, label}) => (
+                <li key={href}>
+                  <Link href={href}>{label}</Link>
+                </li>
+              ))}
+            </ul>
           </nav>
         </header>
         <main>{children}</main>
